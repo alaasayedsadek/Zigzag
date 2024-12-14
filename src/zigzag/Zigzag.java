2f6360b1cd44ad6b2278f01ecaa18704d17b4a82
Salah Alkashif <salahalkashif11@gmail.com>
2024-12-15 00:08:52 +0200

    adding image

--- src/zigzag/Zigzag.java
+++ src/zigzag/Zigzag.java
@@ -1,12 +1,5 @@
 package zigzag;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-//package project;
-
 import com.sun.opengl.util.*;
 import java.awt.*;
 import javax.media.opengl.*;
@@ -15,9 +8,7 @@ import javax.swing.*;
 public class Zigzag extends JFrame {
 
     public static void main(String[] args) {
-//        for (int i = 0; i < 10; i++) {
-//            System.out.println(i%4);
-//        }
+
         new Zigzag();
     }
 
@@ -38,7 +29,7 @@ public class Zigzag extends JFrame {
 
         setTitle("Zigzag");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setSize(1000, 1000);
+        setSize(1000, 600);
         setLocationRelativeTo(null);
         setVisible(true);
         setFocusable(true);
